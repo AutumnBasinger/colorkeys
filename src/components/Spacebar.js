5fd21c168ad164104dfb8796f19e1918079fd54a
Autumn Basinger <autumnelise@hotmail.com>
2018-04-04 20:09:31 -0700

    Add spacebar messages

--- src/components/Spacebar.js
+++ src/components/Spacebar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 const spacebarStyle = {
   width: 330,
@@ -11,11 +11,36 @@ const spacebarStyle = {
   borderRadius: 8
 };
 
-export const Spacebar = props => {
-    const messages = ['Adding red','Adding green','Adding blue']
-    let spaces = props.currentText.split(" ").length-1
-    var buttonText = messages[spaces%3]
+const clickMessages = ['WOAH!','HEY THERE!','HAHAHAHA']
+const addingMessages = ['Adding red','Adding green','Adding blue']
+
+export class Spacebar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      type: 'add',
+      clicked: ''}
+    this.handleMouseDown = this.handleMouseDown.bind(this);
+  }
+  sleep(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
+  }
+  handleMouseDown() {
+    this.setState({ type: 'click', clicked: clickMessages[Math.floor(Math.random() * 3)] })
+    this.sleep(400).then(() => {
+      this.setState({type: 'add'})
+    })
+  }
+  render() {
+    let spaces = this.props.currentText.split(" ").length-1
+    if (this.state.type === 'click') {var buttonText = this.state.clicked}
+    else {buttonText = addingMessages[spaces%3]}
     return (
-      <button style={spacebarStyle}>{buttonText}</button>
+      <button
+        style={spacebarStyle}
+        onMouseDown={this.handleMouseDown}>
+          {buttonText}
+      </button>
     );
   }
+}
